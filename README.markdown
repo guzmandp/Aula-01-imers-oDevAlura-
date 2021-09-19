7b9c9ba346a6c86c5fde8092d54c5daf92d83c89
guzmandp <guzmandp@gmail.com>
2021-09-19 11:18:51 -0300

    Aula01 Completed

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média Guzmandp
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/guzmandp/pen/ExXENKV](https://codepen.io/guzmandp/pen/ExXENKV).
+
+
---
+++ src/script.js
@@ -0,0 +1,21 @@
+var nome = "Darwin"
+var notaDoPrimeiroBimestre = 9.4355
+var notaDoSegundoBimestre = 7.234
+var notaDoTerceiroBimestre = 4.234
+var notaDoQuartoBimestre = 2.183
+
+var somaNotaFinal = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
+
+var notaFinal = somaNotaFinal / 4 
+
+
+var notaFixada = notaFinal.toFixed(2)
+
+
+console.log("Ben vindo " + nome)
+console.log(notaFinal)
+console.log(notaFixada)
+
+
+//Revisáo
+//Variáveis, string, console.log, toFixed, operações, matemáticas, contatenação
